d66346d3ada25fe99ab0cd95357c929876f25e05
Matt Jarvis <matt.jarvis@gmail.com>
2020-10-08 17:31:47 +0100

    AEA-365: Adding logging to diagnose a problem.

--- src/main/kotlin/com/example/fhirvalidator/controller/ValidateController.kt
+++ src/main/kotlin/com/example/fhirvalidator/controller/ValidateController.kt
@@ -3,6 +3,8 @@ package com.example.fhirvalidator.controller
 import ca.uhn.fhir.context.FhirContext
 import ca.uhn.fhir.validation.FhirValidator
 import com.example.fhirvalidator.service.MessageDefinitionApplier
+import mu.KLogger
+import mu.KLogging
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -13,9 +15,12 @@ class ValidateController(
         private val validator: FhirValidator,
         private val messageDefinitionApplier: MessageDefinitionApplier
 ) {
+    companion object : KLogging()
+
     @PostMapping("/\$validate", produces = ["application/json"])
     fun validate(@RequestBody input: String): String {
         val jsonParser = fhirContext.newJsonParser()
+        logger.info("Received message:\n$input")
         val inputResource = jsonParser.parseResource(input)
         messageDefinitionApplier.applyMessageDefinition(inputResource)
         val result = validator.validateWithResult(inputResource).toOperationOutcome()
