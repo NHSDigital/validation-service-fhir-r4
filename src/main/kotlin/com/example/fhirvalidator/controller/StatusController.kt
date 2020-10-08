e4902ac4bd338bc53bb003a41ab78dfb2a4ee77b
Matt Jarvis <matt.jarvis@gmail.com>
2020-10-08 15:50:35 +0100

    AEA-365: Add _status endpoint for health check.

---
+++ src/main/kotlin/com/example/fhirvalidator/controller/StatusController.kt
@@ -0,0 +1,12 @@
+package com.example.fhirvalidator.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class StatusController {
+    @GetMapping("_status")
+    fun validate(): String {
+        return "Validator is alive"
+    }
+}
