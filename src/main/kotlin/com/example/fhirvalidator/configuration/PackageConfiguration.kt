5462681d456167ccb87af0be5bb85e954a4a0f26
Matt Jarvis <matt.jarvis@gmail.com>
2020-10-09 15:44:33 +0100

    AEA-627: Use objectMapper for JSON parsing.

--- src/main/kotlin/com/example/fhirvalidator/configuration/PackageConfiguration.kt
+++ src/main/kotlin/com/example/fhirvalidator/configuration/PackageConfiguration.kt
@@ -1,31 +1,27 @@
 package com.example.fhirvalidator.configuration
 
-import com.google.gson.Gson
+import com.example.fhirvalidator.model.SimplifierPackage
+import com.fasterxml.jackson.databind.ObjectMapper
 import mu.KLogging
 import org.hl7.fhir.utilities.cache.NpmPackage
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
+import java.util.*
+import kotlin.streams.toList
 
 @Configuration
-class PackageConfiguration {
+class PackageConfiguration(val objectMapper: ObjectMapper) {
     companion object : KLogging()
-    class Package(var packageName: String, val version: String){}
 
     @Bean
-    fun getPackages(): Array<NpmPackage> {
-        val packages = Gson().fromJson(
-                String(ClassPathResource("manifest.json").inputStream.readAllBytes()),
-                Array<Package>::class.java
-        ).toList()
-
-        val packageList = emptyArray<NpmPackage>()
-
-        packages.forEach{
-            val packageFileName = "${it.packageName}-${it.version}".replace(".", "-") + ".pkg"
-            packageList.plus(NpmPackage.fromPackage(ClassPathResource(packageFileName).inputStream))
-        }
-
-        return packageList
+    fun getPackages(): List<NpmPackage> {
+        val inputStream = ClassPathResource("manifest.json").inputStream
+        val packages = objectMapper.readValue(inputStream, Array<SimplifierPackage>::class.java)
+        return Arrays.stream(packages)
+                .map { "${it.packageName}-${it.version}".replace(".", "-") + ".pkg" }
+                .map { ClassPathResource(it).inputStream }
+                .map { NpmPackage.fromPackage(it) }
+                .toList()
     }
 }
---
+++ src/main/kotlin/com/example/fhirvalidator/model/SimplifierPackage.kt
@@ -0,0 +1,3 @@
+package com.example.fhirvalidator.model
+
+data class SimplifierPackage(var packageName: String, val version: String)
