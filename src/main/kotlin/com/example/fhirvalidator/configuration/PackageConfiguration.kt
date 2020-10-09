5c843118e4484fc20d14dd48c5aaec9155aef3a5
Conner Jowitt <conner.jowitt@infinityworks.com>
2020-10-09 13:55:14 +0100

    first draft at downloading before runtime

--- src/main/kotlin/com/example/fhirvalidator/configuration/PackageConfiguration.kt
+++ src/main/kotlin/com/example/fhirvalidator/configuration/PackageConfiguration.kt
@@ -2,46 +2,34 @@ package com.example.fhirvalidator.configuration
 
 import com.example.fhirvalidator.model.SimplifierPackageVersion
 import com.example.fhirvalidator.model.SimplifierPackageVersionListing
+import com.google.gson.Gson
 import mu.KLogging
 import org.hl7.fhir.utilities.cache.NpmPackage
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.Resource
 import org.springframework.web.client.RestTemplate
 
 @Configuration
 class PackageConfiguration {
     companion object : KLogging()
+    class Package(var packageName: String, val version: String){}
 
     @Bean
-    fun ukDmPackage(restTemplate: RestTemplate): NpmPackage? {
-        return downloadSimplifierPackageLatestVersion(restTemplate, "UK.DM.r4")
-    }
+    fun getPackages(): Array<NpmPackage> {
+        val packages = Gson().fromJson(
+                String(ClassPathResource("manifest.json").inputStream.readAllBytes()),
+                Array<Package>::class.java
+        ).toList()
 
-    @Bean
-    fun ukSpinePackage(restTemplate: RestTemplate): NpmPackage? {
-        return downloadSimplifierPackageLatestVersion(restTemplate, "UK.Spine.r4")
-    }
+        val packageList = emptyArray<NpmPackage>()
 
-    @Bean
-    fun ukCorePackage(restTemplate: RestTemplate): NpmPackage? {
-        return downloadSimplifierPackageLatestVersion(restTemplate, "UK.Core.r4")
-    }
+        packages.forEach{
+            val name = "${it.packageName}-${it.version}".replace(".", "-") + ".pkg"
+            packageList.plus(NpmPackage.fromPackage(ClassPathResource(name).inputStream))
+        }
 
-    private fun downloadSimplifierPackageLatestVersion(restTemplate: RestTemplate, packageName: String): NpmPackage? {
-        logger.info("Downloading manifest for package $packageName")
-        val manifest = restTemplate.getForObject("https://packages.simplifier.net/$packageName", SimplifierPackageVersionListing::class.java)
-        val latestVersion = manifest?.versions?.values?.last()
-        return latestVersion?.let { downloadSimplifierPackage(restTemplate, latestVersion) }
-    }
-
-    private fun downloadSimplifierPackage(restTemplate: RestTemplate, version: SimplifierPackageVersion): NpmPackage? {
-        return downloadSimplifierPackage(restTemplate, version.name, version.version)
-    }
-
-    private fun downloadSimplifierPackage(restTemplate: RestTemplate, packageName: String, packageVersion: String): NpmPackage? {
-        logger.info("Downloading package $packageName version $packageVersion")
-        val responseEntity = restTemplate.getForEntity("https://packages.simplifier.net/$packageName/$packageVersion", Resource::class.java)
-        return responseEntity.body?.inputStream?.let { NpmPackage.fromPackage(it) }
+        return packageList
     }
 }
