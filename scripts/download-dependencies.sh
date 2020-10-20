3a3d49720c87e97ac0f082f98d5df05f23e91cab
Conner Jowitt <conner.jowitt@infinityworks.com>
2020-10-20 13:35:41 +0100

    added make command to get latest packages from simplifier

--- scripts/download-dependencies.sh
+++ scripts/download-dependencies.sh
@@ -4,7 +4,7 @@ rm packages.txt
 jq -r '.[] | ("https://packages.simplifier.net/" + .packageName + "/" + .version)' <./manifest.json >>packages.txt
 while IFS="" read -r p || [ -n "$p" ]
 do
-  filename=$(echo "$p" | sed -e 's/https\:\/\/packages\.simplifier\.net\///' -e 's/[\.\/]/-/g')
+  filename=$(echo "$p" | sed -e 's/https\:\/\/packages\.simplifier\.net\///' -e 's/\/.*//' -e 's/[\.\/]/-/g')
   curl -o "./$filename.pkg" "$p"
 done < packages.txt
 rm packages.txt
