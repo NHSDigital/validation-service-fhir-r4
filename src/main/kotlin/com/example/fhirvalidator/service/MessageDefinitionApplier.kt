abee4b0b5ccf2d4c0386db4d7bb67f225140f173
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2021-06-25 05:43:45 +0100

    Reactivated message definition validation

--- src/main/kotlin/com/example/fhirvalidator/service/MessageDefinitionApplier.kt
+++ src/main/kotlin/com/example/fhirvalidator/service/MessageDefinitionApplier.kt
@@ -14,11 +14,9 @@ class MessageDefinitionApplier(
     implementationGuideParser: ImplementationGuideParser,
     npmPackages: List<NpmPackage>
 ) {
-    /*
     val messageDefinitions = npmPackages.map(implementationGuideParser::getMessageDefinitions).flatten()
-*/
+
     fun applyMessageDefinition(resource: IBaseResource): OperationOutcome? {
-        /*
         if (resource !is Bundle || resource.type != Bundle.BundleType.MESSAGE) {
             return null
         }
@@ -37,11 +35,8 @@ class MessageDefinitionApplier(
             )
 
         return applyMessageDefinition(resource, messageDefinition)
-
-     */
-        return null
     }
-/*
+
     private fun findMessageHeader(bundle: Bundle): MessageHeader? {
         return bundle.entry
             ?.map { it.resource }
@@ -115,5 +110,5 @@ class MessageDefinitionApplier(
 
         return null
     }
-*/
+
 }
