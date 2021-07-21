966d440308dec268546e9a57382e9db3a7a4700d
callumtipper-IW <callum.tipper@infinityworks.com>
2021-07-21 17:53:17 +0100

    added a validation step to handle searchsets as inputResources

--- src/main/kotlin/com/example/fhirvalidator/controller/ValidateController.kt
+++ src/main/kotlin/com/example/fhirvalidator/controller/ValidateController.kt
@@ -8,6 +8,9 @@ import com.example.fhirvalidator.service.MessageDefinitionApplier
 import com.example.fhirvalidator.util.createOperationOutcome
 import mu.KLogging
 import org.hl7.fhir.instance.model.api.IBaseOperationOutcome
+import org.hl7.fhir.instance.model.api.IBaseResource
+import org.hl7.fhir.r4.model.Bundle
+import org.hl7.fhir.r4.model.OperationOutcome
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
@@ -33,15 +36,35 @@ class ValidateController(
         return fhirContext.newJsonParser().encodeResourceToString(result)
     }
 
-    private fun parseAndValidateResource(input: String): IBaseOperationOutcome {
+    private fun parseAndValidateResource(input: String): OperationOutcome {
         return try {
             val inputResource = fhirContext.newJsonParser().parseResource(input)
-            val messageDefinitionErrors = messageDefinitionApplier.applyMessageDefinition(inputResource)
-            capabilityStatementApplier.applyCapabilityStatementProfiles(inputResource)
-            messageDefinitionErrors ?: validator.validateWithResult(inputResource).toOperationOutcome()
+            val resources = getResourcesToValidate(inputResource)
+            val operationOutcomeList = resources.map { validateResource(it) }
+            val operationOutcomeIssues = operationOutcomeList.filterNotNull().flatMap { it.issue }
+            return createOperationOutcome(operationOutcomeIssues)
         } catch (e: DataFormatException) {
             logger.error("Caught parser error", e)
             createOperationOutcome("Invalid JSON", null)
+        }
+    }
+
+    private fun validateResource(resource: IBaseResource): OperationOutcome? {
+        val messageDefinitionErrors = messageDefinitionApplier.applyMessageDefinition(resource)
+        if (messageDefinitionErrors != null) {
+            return messageDefinitionErrors
+        }
+        capabilityStatementApplier.applyCapabilityStatementProfiles(resource)
+        return validator.validateWithResult(resource).toOperationOutcome() as? OperationOutcome
+    }
+
+    private fun getResourcesToValidate(inputResource: IBaseResource?): List<IBaseResource> {
+        return if (inputResource == null) {
+            emptyList()
+        } else if (inputResource is Bundle && inputResource.type == Bundle.BundleType.SEARCHSET) {
+            inputResource.entry.map { it.resource }
+        } else {
+            listOf(inputResource)
         }
     }
 }
