469a1c79719500912e0f5c5f1b70dfea582ecb34
Dan Jones <dan.jones@infinityworks.com>
2021-08-04 14:19:09 +0100

    remove explicit servlet registration

--- src/main/kotlin/com/example/fhirvalidator/FhirValidatorApplication.kt
+++ src/main/kotlin/com/example/fhirvalidator/FhirValidatorApplication.kt
@@ -2,8 +2,10 @@ package com.example.fhirvalidator
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.ServletComponentScan
 
 @SpringBootApplication
+@ServletComponentScan
 class FhirValidatorApplication
 
 fun main(args: Array<String>) {
