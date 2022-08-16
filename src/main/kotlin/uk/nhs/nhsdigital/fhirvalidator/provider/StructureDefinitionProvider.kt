c39d220ee2b6fc6904df29c64eb64cc657944f81
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2022-08-16 14:58:14 +0100

    Returns profile with snapshot

--- src/main/kotlin/uk/nhs/nhsdigital/fhirvalidator/provider/StructureDefinitionProvider.kt
+++ src/main/kotlin/uk/nhs/nhsdigital/fhirvalidator/provider/StructureDefinitionProvider.kt
@@ -5,6 +5,8 @@ import ca.uhn.fhir.rest.annotation.RequiredParam
 import ca.uhn.fhir.rest.annotation.Search
 import ca.uhn.fhir.rest.param.TokenParam
 import ca.uhn.fhir.rest.server.IResourceProvider
+import ca.uhn.fhir.validation.FhirValidator
+import org.hl7.fhir.common.hapi.validation.support.ValidationSupportChain
 import org.hl7.fhir.r4.model.StructureDefinition
 import org.hl7.fhir.utilities.npm.NpmPackage
 import org.springframework.beans.factory.annotation.Qualifier
@@ -12,7 +14,9 @@ import org.springframework.stereotype.Component
 import uk.nhs.nhsdigital.fhirvalidator.service.ImplementationGuideParser
 
 @Component
-class StructureDefinitionProvider (@Qualifier("R4") private val fhirContext: FhirContext, private val npmPackages: List<NpmPackage>) : IResourceProvider {
+class StructureDefinitionProvider (
+    @Qualifier("R4") private val fhirContext: FhirContext,
+    private val supportChain : ValidationSupportChain) : IResourceProvider {
     /**
      * The getResourceType method comes from IResourceProvider, and must
      * be overridden to indicate what type of resource this provider
@@ -28,17 +32,12 @@ class StructureDefinitionProvider (@Qualifier("R4") private val fhirContext: Fhi
     @Search
     fun search(@RequiredParam(name = StructureDefinition.SP_URL) url: TokenParam): List<StructureDefinition> {
         val list = mutableListOf<StructureDefinition>()
-        for (npmPackage in npmPackages) {
-            if (!npmPackage.name().equals("hl7.fhir.r4.core")) {
-                for (resource in implementationGuideParser!!.getResourcesOfTypeFromPackage(
-                    npmPackage,
-                    StructureDefinition::class.java
-                )) {
-                    if (resource.url.equals(url.value)) {
-                        resource.setId(url.value);
-                        list.add(resource)
-                    }
-                }
+        for (resource in supportChain.fetchAllStructureDefinitions()) {
+            val structureDefinition = resource as StructureDefinition
+            if (structureDefinition.url.equals(url.value)) {
+                resource.setId(url.value);
+
+                list.add(structureDefinition)
             }
         }
         return list
