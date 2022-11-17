9ecba1fdadff8020b4388008716e35ed8e28a4b6
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2022-11-17 17:41:48 +0000

    Copied CORS filter from CCRI

--- src/main/kotlin/uk/nhs/nhsdigital/fhirvalidator/configuration/ApplicationConfiguration.kt
+++ src/main/kotlin/uk/nhs/nhsdigital/fhirvalidator/configuration/ApplicationConfiguration.kt
@@ -1,11 +1,15 @@
 package uk.nhs.nhsdigital.fhirvalidator.configuration
 
 import ca.uhn.fhir.context.FhirContext
-import ca.uhn.fhir.context.FhirVersionEnum
 import ca.uhn.fhir.parser.StrictErrorHandler
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import uk.nhs.nhsdigital.fhirvalidator.util.CorsFilter
+import javax.servlet.Filter
 
 @Configuration
 open class ApplicationConfiguration {
@@ -40,5 +44,19 @@ open class ApplicationConfiguration {
     @Bean
     open fun restTemplate(): RestTemplate {
         return RestTemplate()
+    }
+
+    @Bean
+    fun corsFilter(): FilterRegistrationBean<*>? {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        source.registerCorsConfiguration("/**", config)
+        val bean: FilterRegistrationBean<*> = FilterRegistrationBean<Filter>(/* filter = */ CorsFilter())
+        bean.order = 0
+        return bean
     }
 }
---
+++ src/main/kotlin/uk/nhs/nhsdigital/fhirvalidator/util/CorsFilter.kt
@@ -0,0 +1,34 @@
+package uk.nhs.nhsdigital.fhirvalidator.util
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpHeaders
+import java.io.IOException
+import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class CorsFilter :Filter {
+
+
+    @Throws(IOException::class, ServletException::class)
+    override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
+        val response = res as HttpServletResponse
+        val request = req as HttpServletRequest
+   //     log.debug("Cors Filter: {}", request.method)
+        response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+        response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "POST, PUT, GET, OPTIONS, DELETE")
+        response.setHeader(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600")
+        response.setHeader(
+            HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,
+            "X-FHIR-Starter,authorization,Prefer,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers"
+        )
+        if ("OPTIONS" != request.method) {
+            chain.doFilter(req, res)
+        }
+    }
+
+    override fun init(filterConfig: FilterConfig?) {}
+
+    override fun destroy() {}
+}
