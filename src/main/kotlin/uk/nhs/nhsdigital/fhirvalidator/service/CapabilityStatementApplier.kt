fab90c240708fd063c3c84b0366b63fdf469d4c6
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2022-12-22 04:55:17 +0000

    Added examples to OAS spec and early work on json to plain text conversion

--- src/main/kotlin/uk/nhs/nhsdigital/fhirvalidator/service/CapabilityStatementApplier.kt
+++ src/main/kotlin/uk/nhs/nhsdigital/fhirvalidator/service/CapabilityStatementApplier.kt
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service
 
 @Service
 class CapabilityStatementApplier(
-    implementationGuideParser: ImplementationGuideParser,
     val supportChain: ValidationSupportChain
 ) {
     private val restResources = supportChain.fetchAllConformanceResources()?.filterIsInstance(CapabilityStatement::class.java)
