ab5fc9b63ad7faaaccfc3d01fe943775a54ab242
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-04-12 17:10:45 +0000

    rename

--- src/main/kotlin/software/nhs/fhirvalidator/webserver/FhirValidatorWebserver.kt
+++ src/main/kotlin/software/nhs/fhirvalidator/webserver/FhirValidatorWebserver.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class FhirValidatorApplication
+class FhirValidatorWebserver
 
 fun main(args: Array<String>) {
-    runApplication<FhirValidatorApplication>(*args)
+    runApplication<FhirValidatorWebserver>(*args)
 }
