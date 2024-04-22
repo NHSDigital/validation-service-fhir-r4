7b9b93c2708b6417ba143294e2fa5fe20278eb96
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-04-22 13:07:02 +0000

    test a valid example

--- src/main/kotlin/com/example/fhirvalidator/configuration/ValidationConfiguration.kt
+++ src/main/kotlin/com/example/fhirvalidator/configuration/ValidationConfiguration.kt
@@ -63,7 +63,8 @@ class ValidationConfiguration(private val implementationGuideParser: Implementat
                 theDisplay: String?,
                 theValueSet: IBaseResource
             ): IValidationSupport.CodeValidationResult? {
-                val valueSetUrl = CommonCodeSystemsTerminologyService.getValueSetUrl(fhirContext, theValueSet)
+                //val valueSetUrl = CommonCodeSystemsTerminologyService.getValueSetUrl(fhirContext, theValueSet)
+                val valueSetUrl = CommonCodeSystemsTerminologyService.getValueSetUrl(theValueSet)
 
                 if (valueSetUrl == "https://fhir.nhs.uk/ValueSet/NHSDigital-MedicationRequest-Code"
                     || valueSetUrl == "https://fhir.nhs.uk/ValueSet/NHSDigital-MedicationDispense-Code"
---
+++ src/test/kotlin/com/example/fhirvalidator/controller/FhirExamplesTest.kt
@@ -0,0 +1,51 @@
+package com.example.fhirvalidator.controller
+
+import org.hl7.fhir.r4.model.OperationOutcome
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.Named
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.Arguments
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.opentest4j.AssertionFailedError
+import java.io.File
+import java.util.stream.Stream
+import javax.print.attribute.standard.Severity
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class FhirExamplesTest {
+    val loader = Thread.currentThread().getContextClassLoader()
+
+    @Autowired
+    lateinit var testValidateController: ValidateController
+
+    fun getFiles(): Stream<Arguments> {
+        val dir = File( loader.getResource("examples").file)
+        val exampleFiles = dir.walk()
+            .filter { it.isFile }
+            .toList()
+            .map { Arguments.of(Named.of("parseAndValidateResource should return no errors for file ${it.absolutePath}", it)) }
+            .stream()
+        return exampleFiles
+    }
+
+    @DisplayName("Test all valid example files")
+    @ParameterizedTest
+    @MethodSource("getFiles")
+    fun testExample(exampleFile: File) {
+        val lines = exampleFile.bufferedReader().readLines()
+        val fileContent = lines.joinToString(" ")
+        val actualResult = testValidateController.parseAndValidateResource(fileContent)
+        for (issue in actualResult.issue) {
+            if (issue.severity.equals(OperationOutcome.IssueSeverity.ERROR)) {
+                throw AssertionFailedError("Error found checking file ${exampleFile.absolutePath}. Error: ${issue.diagnostics}")
+            }
+            assertNotEquals(issue.severity, OperationOutcome.IssueSeverity.ERROR)
+        }
+    }
+
+}
