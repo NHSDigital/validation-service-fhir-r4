684853d9d1279d93eb3b3a0814e48bed285e5a75
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-10-25 07:54:37 +0000

    upgrade to 6.8.7

--- src/test/kotlin/com/example/fhirvalidator/controller/FhirExamplesTest.kt
+++ src/test/kotlin/com/example/fhirvalidator/controller/FhirExamplesTest.kt
@@ -27,7 +27,6 @@ internal class FhirExamplesTest {
         val dir = File( loader.getResource("examples").file)
         val exampleFiles = dir.walk()
             .filter { it.isFile }
-            .filter { it.absolutePath == "/workspaces/validation-service-fhir-r4/target/test-classes/examples/primary-care/acute/no-nominated-pharmacy/medical-prescriber/author/gmc/responsible-party/din/1-Process-Request-Send-200_OK.json" }
             .toList()
             .map { Arguments.of(Named.of("parseAndValidateResource should return no errors for file ${it.absolutePath}", it)) }
             .stream()
