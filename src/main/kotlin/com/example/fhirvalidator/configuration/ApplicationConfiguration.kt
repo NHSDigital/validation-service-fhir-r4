86de0cb90b8f58eb2b1029a2b7136b37490972c4
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2025-04-16 15:15:22 +0100

    Fix: [AEA-0000] - add version number to validator status (#231)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - add version info to status endpoint
    - log version info on startup
    - upgrade java version
    - build docker image on pull requests

--- src/main/kotlin/com/example/fhirvalidator/configuration/ApplicationConfiguration.kt
+++ src/main/kotlin/com/example/fhirvalidator/configuration/ApplicationConfiguration.kt
@@ -2,16 +2,25 @@ package com.example.fhirvalidator.configuration
 
 import ca.uhn.fhir.context.FhirContext
 import ca.uhn.fhir.parser.StrictErrorHandler
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
 
 @Configuration
 class ApplicationConfiguration {
+    private val logger = KotlinLogging.logger {}
+
     @Bean
     fun fhirContext(): FhirContext {
         val fhirContext = FhirContext.forR4()
         fhirContext.setParserErrorHandler(StrictErrorHandler())
+
+        val validatorVersion = System.getenv("validatorVersion") ?: "unknown"
+        val commitSha = System.getenv("commitSha") ?: "unknown"
+        logger.info{ "validatorVersion: $validatorVersion"}
+        logger.info{ "commitSha: $commitSha"}
+
         return fhirContext
     }
 
